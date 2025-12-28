4f584910fd5e20c292a636f2b56e612df0957794
asmaakther <71425950+asmaakther@users.noreply.github.com>
2025-12-28 03:09:14 -0500

    Update update_links.py

--- update_links.py
+++ update_links.py
@@ -4,8 +4,13 @@ import os
 
 API_KEY = os.getenv('YOUTUBE_API_KEY')
 # এখানে আপনার ২০টি চ্যানেলের নাম দিন
-CHANNELS = ["Al Jazeera Live","Somoy TV Live", "Ekhon Tv Live","Ekattor TV Live","Jamuna Tv Live","DBC News Live","Channel i Live","ATN News Live","NTV Live","Rtv Live","NEWS24 LIVE","Desh TV Live","independent TV Live","Channel 24 Live",]
-
+CHANNELS = [
+    "Al Jazeera Live", "Somoy TV Live", "Ekhon Tv Live", 
+    "Ekattor TV Live", "Jamuna Tv Live", "DBC News Live", 
+    "Channel i Live", "ATN News Live", "NTV Live", 
+    "Rtv Live", "NEWS24 LIVE", "Desh TV Live", 
+    "independent TV Live", "Channel 24 Live"
+]
 def get_live_id(query):
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&q={query}&key={API_KEY}"
     response = requests.get(url).json()
